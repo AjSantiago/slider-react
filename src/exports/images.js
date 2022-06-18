c200fd30925c04df05be239e678c948a20ac38de
AjSantiago <isc_aj.santiago@hotmail.com>
2022-06-17 19:53:14 -0500

    Commit inicial

---
+++ src/exports/images.js
@@ -0,0 +1,8 @@
+import image1 from '../assets/01.jpg';
+import image2 from '../assets/02.jpg';
+import image3 from '../assets/03.jpg';
+import image4 from '../assets/04.jpg';
+import image5 from '../assets/05.jpg';
+import image6 from '../assets/06.jpg';
+
+export default [image1, image2, image3, image4, image5, image6];
